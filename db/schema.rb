6e31800717443f066f2b9952a06c80794c39240e
rodolforpr88 <rodolforpr88@gmail.com>
2022-03-17 11:18:16 -0300

    solving conflict2

--- db/schema.rb
+++ db/schema.rb
@@ -69,9 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_10_162623) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-=======
->>>>>>> master
 end
