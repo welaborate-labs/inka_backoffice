2ecda0ec91025c239761cca3a250d84dc890cd82
rodolforpr88 <rodolforpr88@gmail.com>
2022-03-21 14:29:08 -0300

    create a helper for currency in the application helper e change the field price to decimal

--- db/migrate/20220317190113_create_services.rb
+++ db/migrate/20220317190113_create_services.rb
@@ -3,10 +3,9 @@ class CreateServices < ActiveRecord::Migration[7.0]
     create_table :services do |t|
       t.string :title
       t.integer :duration
-      t.integer :price
+      t.decimal :price, precision: 8, scale: 2
       t.boolean :is_comissioned, default: false
       t.references :professional, null: false, foreign_key: true
-      # t.references :service, foreign_key: { to_table: :services }
       t.belongs_to :service, foreign_key: true, null: true
       t.timestamps
     end
