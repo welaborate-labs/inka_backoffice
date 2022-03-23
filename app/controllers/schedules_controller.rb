263771cc17f90a77526f5101faca9fe016cde96f
rodolforpr88 <rodolforpr88@gmail.com>
2022-03-23 18:28:42 -0300

    update the schedules controller, add validations on schedule model and refactor some views * Schedule Controller update the param to merge the current_user.professional * Schedule Model Create the Enum for Weeky and also create the validation for the fields (presence and maximum length) * Application Layout Add the importation for the jquery * Professionals/_form remove the importation to jquery * Professionals/_schedule_fields update the weekday field to be a select passing the enum weekyday as options * Schedules/_form the same as Professionals/_schedules_fields

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,5 @@
 class SchedulesController < ApplicationController
-  before_action :set_schedule, only: %i[ show edit update destroy ]
+  before_action :set_schedule, only: %i[show edit update destroy]
 
   # GET /schedules or /schedules.json
   def index
@@ -7,8 +7,7 @@ class SchedulesController < ApplicationController
   end
 
   # GET /schedules/1 or /schedules/1.json
-  def show
-  end
+  def show; end
 
   # GET /schedules/new
   def new
@@ -16,8 +15,7 @@ class SchedulesController < ApplicationController
   end
 
   # GET /schedules/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /schedules or /schedules.json
   def create
@@ -25,7 +23,9 @@ class SchedulesController < ApplicationController
 
     respond_to do |format|
       if @schedule.save
-        format.html { redirect_to schedule_url(@schedule), notice: "Schedule was successfully created." }
+        format.html do
+          redirect_to schedule_url(@schedule), notice: 'Schedule was successfully created.'
+        end
         format.json { render :show, status: :created, location: @schedule }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,9 @@ class SchedulesController < ApplicationController
   def update
     respond_to do |format|
       if @schedule.update(schedule_params)
-        format.html { redirect_to schedule_url(@schedule), notice: "Schedule was successfully updated." }
+        format.html do
+          redirect_to schedule_url(@schedule), notice: 'Schedule was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @schedule }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +54,30 @@ class SchedulesController < ApplicationController
     @schedule.destroy
 
     respond_to do |format|
-      format.html { redirect_to schedules_url, notice: "Schedule was successfully destroyed." }
+      format.html { redirect_to schedules_url, notice: 'Schedule was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_schedule
-      @schedule = Schedule.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def schedule_params
-      params.require(:schedule).permit(:weekday, :starts_at, :ends_at, :interval_starts_at, :interval_ends_at, :professional_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def schedule_params
+    params
+      .require(:schedule)
+      .permit(
+        :weekday,
+        :starts_at,
+        :ends_at,
+        :interval_starts_at,
+        :interval_ends_at,
+        :professional_id
+      )
+      .merge(professional: current_user.professional)
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,26 @@
 class Schedule < ApplicationRecord
   belongs_to :professional
+  enum weekday: %i[monday tuesday wednesday thursday friday]
+
+  # Validations
+  # presence
+  validates :weekday, :starts_at, :ends_at, presence: true
+  validates :interval_starts_at,
+            :interval_ends_at,
+            presence: true,
+            if: lambda { self.interval_starts_at.present? || self.interval_ends_at.present? }
+
+  # length within min..max
+  validates :starts_at, length: { maximum: 15 }, if: lambda { self.starts_at.present? }
+  validates :ends_at, length: { maximum: 15 }, if: lambda { self.ends_at.present? }
+  validates :interval_starts_at,
+            length: {
+              maximum: 15
+            },
+            if: lambda { self.interval_starts_at.present? }
+  validates :interval_ends_at,
+            length: {
+              maximum: 15
+            },
+            if: lambda { self.interval_ends_at.present? }
 end
