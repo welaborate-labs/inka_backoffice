bac1e8e3e5a35f23fb0b622965534185e5ca1ba5
Rodolfo Porfirio Rodrigues <34282341+rodolforpr88@users.noreply.github.com>
2022-04-14 10:11:16 -0300

    refactor the authentication and refactor the error messages (#16)
    * refactor the authentication and refactor the error messages
    
    * app/controllers/identities_controller refactor the new method to show if there are errors and render it
    * app/controllers/sessions_controller set the sessions[:user_id] = @user.id when @user is present
    * config/omniauth set a better config for the 'on failue, on failed registration' methods
    * app/views/shared/error_messages refactor a small issue tha was nothing showing the count of errors
    * app/views/identities* refactor the form for the partial and add the error_message partial in the 'new' view
    
    * fix a loop of redirects removing the redirect to from the line 19

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -3,5 +3,12 @@ class IdentitiesController < ApplicationController
 
   def new
     @identity = request.env['omniauth.identity']
+
+    if @identity&.errors
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @identity.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
