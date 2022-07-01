73d68ca2156ed9628743afc9a872d5c7bd7d1a97
Rodolfo Porfirio Rodrigues <34282341+rodolforpr88@users.noreply.github.com>
2022-07-01 10:26:11 -0300

    refactor the service controller adding the product_usage id as permitted params to allow the destroy (#33)

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ServicesController, type: :controller do
   let(:identity) { create(:identity) }
@@ -7,22 +7,13 @@ RSpec.describe ServicesController, type: :controller do
   let(:main_service) { create(:service) }
   before { allow_any_instance_of(ApplicationController).to receive(:current_user) { user } }
 
-  let(:new_attributes) { { title: 'New Title' } }
-  let(:valid_attributes) do
-    { title: 'Title', duration: 30, price: 45.50 }
-  end
+  let(:new_attributes) { { title: "New Title" } }
+  let(:valid_attributes) { { title: "Title", duration: 30, price: 45.50 } }
   let(:invalid_attributes) { { title: nil } }
-  let(:optional_service) do
-    {
-      title: 'Title',
-      duration: 30,
-      price: 45.50,
-      service_id: main_service.id
-    }
-  end
+  let(:optional_service) { { title: "Title", duration: 30, price: 45.50, service_id: main_service.id } }
 
-  describe 'GET /index' do
-    context 'when authenticated' do
+  describe "GET /index" do
+    context "when authenticated" do
       before do
         service.save!
         get :index
@@ -33,7 +24,7 @@ RSpec.describe ServicesController, type: :controller do
       it { expect(assigns(:services)).to include service }
     end
 
-    context 'when does not authenticated' do
+    context "when does not authenticated" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user) { nil } }
 
       before do
@@ -48,8 +39,8 @@ RSpec.describe ServicesController, type: :controller do
     end
   end
 
-  describe 'GET /new' do
-    context 'when authenticated' do
+  describe "GET /new" do
+    context "when authenticated" do
       before { get :new }
 
       it { expect(response).to render_template(:new) }
@@ -57,7 +48,7 @@ RSpec.describe ServicesController, type: :controller do
       it { expect(assigns(:service)).to be_a_new Service }
     end
 
-    context 'when does not authenticated' do
+    context "when does not authenticated" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user) { nil } }
 
       before { get :new }
@@ -69,40 +60,44 @@ RSpec.describe ServicesController, type: :controller do
     end
   end
 
-  describe 'POST /create' do
-    context 'when authenticated' do
-      context 'with valid parameters' do
-        it 'creates a new service' do
-          expect { post :create, params: { service: valid_attributes } }.to change(Service, :count)
-            .by(01)
+  describe "POST /create" do
+    context "when authenticated" do
+      context "with valid parameters" do
+        it "creates a new service" do
+          expect { post :create, params: { service: valid_attributes } }.to change(Service, :count).by(01)
         end
 
-        it 'creates a new optional service' do
-          expect { post :create, params: { service: optional_service } }.to change(Service, :count)
-            .by(02)
+        it "creates a new optional service" do
+          expect { post :create, params: { service: optional_service } }.to change(Service, :count).by(02)
         end
 
-        it 'returns the successfull message' do
+        it "returns the successfull message" do
           post :create, params: { service: valid_attributes }
           expect(flash[:notice]).to eq "Serviço criado com sucesso."
         end
 
-        it 'redirects to the service index' do
+        it "redirects to the service index" do
           post :create, params: { service: valid_attributes }
           expect(response).to redirect_to(service_url(Service.last))
         end
       end
 
-      context 'with invalid parameters' do
+      context "create service with product usages nested" do
+        let(:product) { create(:product) }
+        let(:product_2) { create(:product) }
+        let(:valid_attributes) { { title: "Title", duration: 30, price: 45.50, product_usages_attributes: [{ product_id: product.id, quantity: 5 }, { product_id: product_2.id, quantity: 15 }] } }
+
+        it { expect { post :create, params: { service: valid_attributes } }.to change(Service, :count).by(1) }
+        it { expect { post :create, params: { service: valid_attributes } }.to change(ProductUsage, :count).by(2) }
+      end
+
+      context "with invalid parameters" do
         before { post :create, params: { service: invalid_attributes } }
-        it 'does not create a new service' do
-          expect { post :create, params: { service: invalid_attributes } }.to change(
-            Service,
-            :count
-          ).by(0)
+        it "does not create a new service" do
+          expect { post :create, params: { service: invalid_attributes } }.to change(Service, :count).by(0)
         end
 
-        it 'should return :unprocessable_entity 422' do
+        it "should return :unprocessable_entity 422" do
           expect(response.status).to eq 422
         end
 
@@ -113,7 +108,7 @@ RSpec.describe ServicesController, type: :controller do
       end
     end
 
-    context 'when does not authenticated' do
+    context "when does not authenticated" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user) { nil } }
 
       before { post :create, params: { service: valid_attributes } }
@@ -125,8 +120,8 @@ RSpec.describe ServicesController, type: :controller do
     end
   end
 
-  describe 'GET /show' do
-    context 'when authenticated' do
+  describe "GET /show" do
+    context "when authenticated" do
       before do
         service.save!
         get :show, params: { id: service }
@@ -137,7 +132,7 @@ RSpec.describe ServicesController, type: :controller do
       it { expect(assigns(:service)).to eq service }
     end
 
-    context 'when does not authenticated' do
+    context "when does not authenticated" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user) { nil } }
 
       before do
@@ -152,8 +147,8 @@ RSpec.describe ServicesController, type: :controller do
     end
   end
 
-  describe 'GET /edit' do
-    context 'when authenticated' do
+  describe "GET /edit" do
+    context "when authenticated" do
       before do
         service.save!
         get :edit, params: { id: service }
@@ -162,9 +157,19 @@ RSpec.describe ServicesController, type: :controller do
       it { expect(response).to render_template(:edit) }
       it { expect(response).to be_successful }
       it { expect(assigns(:service)).to eq service }
+
+      describe "edit service with product usages nested" do
+        let(:product) { create(:product) }
+        let(:product_2) { create(:product) }
+        let(:product_usage) { create(:product_usage, service_id: service.id, product_id: product.id) }
+        let(:product_usage_2) { create(:product_usage, service_id: service.id, product_id: product_2.id) }
+
+        it { expect(assigns(:service)).to eq service }
+        it { expect(service.product_usages).to eq [product_usage, product_usage_2] }
+      end
     end
 
-    context 'when does not authenticated' do
+    context "when does not authenticated" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user) { nil } }
 
       before do
@@ -179,46 +184,57 @@ RSpec.describe ServicesController, type: :controller do
     end
   end
 
-  describe 'PUT /update' do
-    context 'when authenticate' do
-      context 'with valid parameters' do
+  describe "PUT /update" do
+    context "when authenticate" do
+      context "with valid parameters" do
         before do
           service.save!
           patch :update, params: { id: service, service: new_attributes }
         end
 
-        it 'should assigns the service' do
+        context "update service with product usages nested" do
+          let(:product) { create(:product) }
+          let(:product_2) { create(:product) }
+          let(:product_usage) { create(:product_usage, service_id: service.id, product_id: product.id) }
+          let(:product_usage_2) { create(:product_usage, service_id: service.id, product_id: product_2.id) }
+          # let(:new_attributes) do
+          #   { title: "Title_updated", product_usages_attributes: { "0": { id: product_usage.id, quantity: 100, _destroy: "false" }, "1": { id: product_usage_2.id, _destroy: "false" } } }
+          # end
+          it { expect(assigns(:service)).to eq service }
+          it { expect(service.product_usages).to eq [product_usage, product_usage_2] }
+          it { expect { service.reload }.to change { service.title }.from("Title Service").to("New Title") }
+        end
+
+        it "should assigns the service" do
           expect(assigns(:service)).to eq service
         end
 
-        it 'updates the requested service' do
-          expect { service.reload }.to change { service.title }
-            .from('Title Service')
-            .to('New Title')
+        it "updates the requested service" do
+          expect { service.reload }.to change { service.title }.from("Title Service").to("New Title")
         end
 
-        it 'redirects to the service' do
+        it "redirects to the service" do
           expect(service.reload).to redirect_to(service_url(service.id))
         end
 
-        it 'returns a flash message' do
+        it "returns a flash message" do
           expect(flash[:notice]).to eq "Serviço atualizado com sucesso."
         end
       end
 
-      describe 'invalid attributes' do
+      describe "invalid attributes" do
         before do
           service.save
           patch :update, params: { id: service, service: { title: nil } }
           service.reload
         end
 
-        it { expect(service.title).to eq('Title Service') }
+        it { expect(service.title).to eq("Title Service") }
         it { expect(service.title).not_to eq(nil) }
       end
     end
 
-    context 'when does not authenticated' do
+    context "when does not authenticated" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user) { nil } }
 
       before do
@@ -233,19 +249,31 @@ RSpec.describe ServicesController, type: :controller do
     end
   end
 
-  describe 'DELETE /destroy' do
-    context 'when authenticated' do
+  describe "DELETE /destroy" do
+    context "when authenticated" do
       before { service.save }
 
       it { expect { delete :destroy, params: { id: service } }.to change(Service, :count).by(-1) }
 
-      it 'returns the flash message' do
+      it "returns the flash message" do
         delete :destroy, params: { id: service }
         expect(flash[:notice]).to eq "Serviço removido com sucesso."
       end
+
+      context "update service with product usages nested" do
+        before { delete :destroy, params: { id: service } }
+
+        let(:product) { create(:product) }
+        let(:product_2) { create(:product) }
+        let(:product_usage) { create(:product_usage, service_id: service.id, product_id: product.id) }
+        let(:product_usage_2) { create(:product_usage, service_id: service.id, product_id: product_2.id) }
+
+        it { expect(assigns(:service)).to eq service }
+        it { expect(service.product_usages).to be_empty }
+      end
     end
 
-    context 'when does not authenticated' do
+    context "when does not authenticated" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user) { nil } }
 
       before do
