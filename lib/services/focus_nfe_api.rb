fdaa00899b6fa87a1176297d7ac4d8fa842a1844
Rodolfo Porfirio <rodolforpr88@gmail.com>
2022-08-16 10:14:32 -0300

    refactor the ur_api and token to constantes and add Signed Global ID for the refs

--- lib/services/focus_nfe_api.rb
+++ lib/services/focus_nfe_api.rb
@@ -2,22 +2,22 @@ require "net/http"
 require "uri"
 
 class FocusNfeApi
+  URL_API = ENV['FOCUSNFE_URL']
+  TOKEN = ENV['FOCUSNFE_KEY']
+
   def initialize(booking)
     @booking = booking
-    @token = ENV['FOCUSNFE_KEY']
-    @url_api = ENV['FOCUSNFE_URL']
   end
 
   def create
-    url = URI(@url_api + "v2/nfse?ref=" + @booking.id.to_s)
+    url = URI(URL_API + "v2/nfse?ref=" + @booking.to_sgid(expires_in: nil).to_s)
 
     https = Net::HTTP.new(url.hostname, url.port)
     https.use_ssl = true
 
     request = Net::HTTP::Post.new(url)
-    request["Authorization"] = "Basic " + Base64.encode64(@token).strip
+    request["Authorization"] = "Basic " + Base64.encode64(TOKEN).strip
     request["Content-Type"] = "application/json"
-
 
     request.body = JSON.dump({
       "data_emissao": @booking.created_at.to_s,
@@ -35,7 +35,7 @@ class FocusNfeApi
           "numero": @booking.customer.number.to_s,
           "complemento": @booking.customer.complement,
           "bairro": @booking.customer.district,
-          "codigo_municipio": "3530607", # valor fixo
+          "codigo_municipio": "3530607",
           "uf": @booking.customer.state,
           "cep": @booking.customer.zip_code.to_s
         }
@@ -47,7 +47,7 @@ class FocusNfeApi
         "item_lista_servico": "0601",
         "codigo_tributario_municipio": "620910000",
         "valor_servicos": @booking.service.price,
-        "codigo_municipio": "3530607" # valor fixo
+        "codigo_municipio": "3530607"
       }
     })
 
@@ -56,21 +56,21 @@ class FocusNfeApi
   end
 
   def show
-    url = URI(@url_api + "v2/nfse/" + @booking.id.to_s)
+    url = URI(URL_API + "v2/nfse/" + @booking.to_sgid(expires_in: nil).to_s)
 
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
 
-    request["Authorization"] = "Basic " + Base64.encode64(@token).strip
+    request["Authorization"] = "Basic " + Base64.encode64(TOKEN).strip
 
     response = https.request(request)
     response.body
   end
 
   def cancel justification
-    url = URI(@url_api + "v2/nfse/" + @booking.id.to_s)
+    url = URI(URL_API + "v2/nfse/" + @booking.to_sgid(expires_in: nil).to_s)
 
     canceled_justification = {
       justificativa: justification.to_s
@@ -81,7 +81,7 @@ class FocusNfeApi
 
     request = Net::HTTP::Delete.new(url)
 
-    request["Authorization"] = "Basic " + Base64.encode64(@token).strip
+    request["Authorization"] = "Basic " + Base64.encode64(TOKEN).strip
 
     request.body = canceled_justification.to_json
 
