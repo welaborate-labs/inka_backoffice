a20e002ee0f8e53bbee25cd0fe1763d222c6a8b7
Rodolfo Porfirio <rodolforpr88@gmail.com>
2022-08-19 08:35:37 -0300

    fix the structure

--- app/jobs/get_nfse_job.rb
+++ app/jobs/get_nfse_job.rb
@@ -5,18 +5,18 @@ class GetNfseJob < ApplicationJob
 
   after_perform do |job|
     case @response["status"]
-      when "nao_encontrado"
-      when "nfe_cancelada"
-      when "nfe_nao_autorizada"
-      when "requisicao_invalida"
-        # notify the wrong fields
-      when "empresa_nao_habilitada"
-        # notify email the company about
-      when "certificado_vencido"
-        # notify email the company about
-      when "nfe_autorizada"
-        # save the xml or url
-      when "em_processamento"
+    when "nao_encontrado"
+    when "nfe_cancelada"
+    when "nfe_nao_autorizada"
+    when "requisicao_invalida"
+      # notify the wrong fields
+    when "empresa_nao_habilitada"
+      # notify via email the company
+    when "certificado_vencido"
+      # notify via email the company
+    when "nfe_autorizada"
+      # save the xml or url and send email
+    when "em_processamento"
       GetNfseJob.set(wait: 5.minutes).perform_later(job.arguments.first)
     else
       @response
